1d6d61a91d01653ab73c49b20cf5a3a46acc7386
DorinRuga <kris.bumford@gmail.com>
2021-11-06 00:03:55 -0700

    fin assignment

--- Week_10/homework/script.js
+++ Week_10/homework/script.js
@@ -1,6 +1,31 @@
-let imgBtn = document.getElementById("imagebutton");
-let present = document.getElementById("presentation");
-let menuBtn = document.getElementById("menubutton")
+let p = document.getElementById("p")
+let menbttn = document.getElementById("menubutton")
 
-imgBtn .addEventListener("click", function(){});
-menuBtn .addEventListener("click", function(){});
+function func(image){
+    p.src = image.src;
+    p.parentElement.style.display="block";
+}
+
+function myFunction() {
+    let nav = document.getElementById("navigation");
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+  }
+
+let toggleImage = function(){
+    if (menbttn.alt == "menu_open") {
+        menbttn.src = "menu_close.png";
+        menbttn.alt = "menu_close";
+    }
+    else {
+        (menbttn.src = "menu_open.png")
+         menbttn.alt = "menu_open";
+     }
+    
+}
+
+
+menbttn.addEventListener("click", toggleImage);
