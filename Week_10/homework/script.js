00fec6637999a488540f5613673a9f453a9578f0
DorinRuga <kris.bumford@gmail.com>
2021-11-05 21:32:47 -0700

    working on homework

---
+++ Week_10/homework/script.js
@@ -0,0 +1,6 @@
+let imgBtn = document.getElementById("imagebutton");
+let present = document.getElementById("presentation");
+let menuBtn = document.getElementById("menubutton")
+
+imgBtn .addEventListener("click", function(){});
+menuBtn .addEventListener("click", function(){});
