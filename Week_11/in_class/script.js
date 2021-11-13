0f1ee6abb59ec8acbf10e30069cea3dba7e86620
DorinRuga <kris.bumford@gmail.com>
2021-11-12 19:18:29 -0800

    added images of veg

--- Week_11/in_class/script.js
+++ Week_11/in_class/script.js
@@ -1,32 +1,52 @@
+//for the carousel:
 let carouselImages = ["quokkas1.jpg", "quokkas2.jpg", "quokkas3.jpg"]; 
 let currentIndex = 2;
 let carouselIMG = document.getElementById ("carouselIMG");
 
+//for the select:
+let groupSelect = document.getElementById("groupSelect");
+let categorySelect = document.getElementById("categorySelect");
+let animalOptions = ['puppies', 'kittens', 'quokkas'];
+let foodOptions = ['coffee', 'desserts', 'cocktails', 'muffins']
+let natureOptions = ['beaches', 'forests', 'lakes'];
+
 let changeslide = function(){
     if(currentIndex === 0){
          carouselIMG.src = carouselImages[1];
-         //currentIndex = 1;
+         currentIndex = 1;
     }
     else if(currentIndex === 1) {
          carouselIMG.src = carouselImages[2];
-         //currentIndex = 2;
+         currentIndex = 2;
     }
     else {
          carouselIMG.src = carouselImages[0];
-         //currentIndex = 0;
-    }
-    if(currentIndex === 2) {
          currentIndex = 0;
     }
+}
+
+let updateCategory = function(){
     
-    if(currentIndex ===2) {
-        currentIndex = 0;
+    categorySelect.innerHTML = '';
+    let selectedValue = groupSelect.value;
+    if(selectedValue === "animals"){
+        animalOptions.forEach(function(elem){
+            categorySelect.innerHTML += '<option value="'+elem+'">' +elem+'</option>';
+        })
     }
-    else {
-         currentIndex = currentIndex+1;
+    else if(selectedValue === "food"){
+        foodOptions.forEach(function(elem){
+            categorySelect.innerHTML += '<option value="'+elem+'">' +elem+'</option>';
+        })
+    }
+    else if(selectedValue === "nature"){
+        natureOptions.forEach(function(elem){
+            categorySelect.innerHTML += '<option value="'+elem+'">' +elem+'</option>';
+        })
     }
 }
-    
+   
+
 window.addEventListener("load", function(){
     //trigger once after 5 sec
     setTimeout(function(){
@@ -35,4 +55,6 @@ window.addEventListener("load", function(){
 
     //triggered every 5 sec
     setInterval(changeslide, 5000);
-})
+});
+
+groupSelect.addEventListener("change", updateCategory)
