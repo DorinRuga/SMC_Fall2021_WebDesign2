f12f982ede9e6cdb7c6e52732f3a43e089f74b8c
DorinRuga <kris.bumford@gmail.com>
2021-11-16 19:07:31 -0800

    fixed

--- Week_10/homework/script.js
+++ Week_10/homework/script.js
@@ -1,28 +1,25 @@
 let p = document.getElementById("p")
 let menbttn = document.getElementById("menubutton")
+let nav = document.getElementById("navigation");
 
 function func(image){
     p.src = image.src;
     p.parentElement.style.display="block";
 }
 
-function myFunction() {
-    let nav = document.getElementById("navigation");
-    if (nav.style.display === "block") {
-      nav.style.display = "none";
-    } else {
-      nav.style.display = "block";
-    }
-  }
-
 let toggleImage = function(){
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
     if (menbttn.alt == "menu_open") {
         menbttn.src = "menu_close.png";
         menbttn.alt = "menu_close";
     }
     else {
-        (menbttn.src = "menu_open.png")
-         menbttn.alt = "menu_open";
+        menbttn.src = "menu_open.png";
+        menbttn.alt = "menu_open";
      }
     
 }
