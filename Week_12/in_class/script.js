85868d5792d2dcd246f8d515fb2f667ca3380ad6
DorinRuga <kris.bumford@gmail.com>
2021-11-22 19:28:00 -0800

    console log

--- Week_12/in_class/script.js
+++ Week_12/in_class/script.js
@@ -2,14 +2,14 @@ let cocktailButton = document.getElementById('fetchCocktail')
 let cocktailDisplay = document.getElementById ("displayCocktail")
 
 let fetchCocktail = function (){
-    let randCocktailURL="";
+    let randCocktailURL="https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
     fetch(randCocktailURL).
         then(function(result){
             return result.json();
         }).
-        then(function(data)
-
+        then(function(data){
+            console.log(data);
         })
 }
 
