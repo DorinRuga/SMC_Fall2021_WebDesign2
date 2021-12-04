a2d00b6ab76f15157c05f57791dd30141ab3fd1e
DorinRuga <kris.bumford@gmail.com>
2021-12-03 22:19:58 -0800

    finished navigation bar

--- Final/02_design_solutions/script.js
+++ Final/02_design_solutions/script.js
@@ -1,6 +1,23 @@
-let toggleMenu = document.getElementById('togglemenu');
-let navlist = document.getElementById('navlist')
 
-toggleMenu.addEventListener('click', () => {
-    navlist.classlist.toggle('active');
-})
+let menbttn = document.getElementById("menubutton")
+let nav = document.getElementById("navigation");
+
+let toggleImage = function(){
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+    if (menbttn.alt == "menu_open") {
+        menbttn.src = "images/menu_close.png";
+        menbttn.alt = "menu_close";
+    }
+    else {
+        menbttn.src = "images/menu_open.png";
+        menbttn.alt = "menu_open";
+     }
+    
+}
+
+
+menbttn.addEventListener("click", toggleImage);
